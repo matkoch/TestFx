77ee1cdea25932569a8481c7e04b8d702009f242
Matthias Koch <ithrowexceptions@gmail.com>
2015-05-11 09:22:55 +0200

    Added TopologicalSort.

---
+++ src/TestFx.Core/Utilities/Collections/UndefinedOrderException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.Serialization;
+
+namespace TestFx.Utilities.Collections
+{
+  /// <summary>
+  /// Exception that is thrown when ordering a set of items where the order is not totally defined.
+  /// </summary>
+  public class UndefinedOrderException : Exception
+  {
+    private const string c_message = "Undefined order for items:\r\n";
+
+    public UndefinedOrderException (IEnumerable items)
+    {
+      Items = items;
+    }
+
+    protected UndefinedOrderException (SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+    }
+
+    public IEnumerable Items { get; private set; }
+
+    public override string Message
+    {
+      get { return c_message + Items; }
+    }
+  }
+}
