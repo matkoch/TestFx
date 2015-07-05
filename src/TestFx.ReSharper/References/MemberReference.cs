bcecfb6b621c1a676c3ecdf50084e5fafec0b142
Matthias Koch <ithrowexceptions@gmail.com>
2015-07-05 03:01:02 +0200

    Added MemberReference facilities.

---
+++ src/TestFx.ReSharper/References/MemberReference.cs
@@ -0,0 +1,108 @@
+// Copyright 2015, 2014 Matthias Koch
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using JetBrains.Reflection;
+using JetBrains.ReSharper.Feature.Services.ExternalSources.Utils;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.CSharp.Util;
+using JetBrains.ReSharper.Psi.ExtensionsAPI.Resolve;
+using JetBrains.ReSharper.Psi.Resolve;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.ReSharper.Psi.Util;
+using TestFx.Utilities;
+
+namespace TestFx.ReSharper.References
+{
+  public class MemberReference : TreeReferenceBase<ILiteralExpression>, ICompleteableReference, IReferenceFromStringLiteral
+  {
+    private readonly ICSharpLiteralExpression _literalExpression;
+    private readonly ITypeElement _typeElement;
+
+    public MemberReference (ICSharpLiteralExpression literalExpression, ITypeElement typeElement)
+        : base(literalExpression)
+    {
+      _literalExpression = literalExpression;
+      _typeElement = typeElement;
+    }
+
+    public override ResolveResultWithInfo ResolveWithoutCache ()
+    {
+      return GetReferenceSymbolTable(true).GetResolveResult(GetName());
+    }
+
+    public override string GetName ()
+    {
+      return _literalExpression.ConstantValue.Value as string ?? "???";
+    }
+
+    public override ISymbolTable GetReferenceSymbolTable (bool useReferenceName)
+    {
+      var symbolTable = ResolveUtil.GetSymbolTableByTypeElement(_typeElement, SymbolTableMode.FULL, _typeElement.Module)
+          .Distinct().Filter(IsDeclaredTypeMember);
+
+      return useReferenceName
+          ? symbolTable.Filter(x => x.ShortName == GetName())
+          : symbolTable;
+    }
+
+    private bool IsDeclaredTypeMember (ISymbolInfo x)
+    {
+      var typeMember = x.GetDeclaredElement() as ITypeMember;
+      return typeMember != null && !typeMember.GetContainingType().IsObjectClass()
+             && !typeMember.HasAttributeInstance(PredefinedType.COMPILER_GENERATED_ATTRIBUTE_CLASS, false);
+    }
+
+    public override TreeTextRange GetTreeTextRange ()
+    {
+      var treeRange = _literalExpression.GetStringLiteralContentTreeRange();
+      return treeRange.Length > 0 ? treeRange : _literalExpression.GetTreeTextRange();
+    }
+
+    public override IReference BindTo (IDeclaredElement element)
+    {
+      var literalAlterer = StringLiteralAltererUtil.CreateStringLiteralByExpression(_literalExpression);
+      literalAlterer.Replace(GetName(), element.ShortName, _literalExpression.GetPsiModule());
+      var newLiteralExpression = literalAlterer.Expression;
+      if (_literalExpression.Equals(newLiteralExpression))
+        return this;
+
+      return newLiteralExpression.FindReference<MemberReference>() ?? this;
+    }
+
+    public override IReference BindTo (IDeclaredElement element, ISubstitution substitution)
+    {
+      return BindTo(element);
+    }
+
+    public override IAccessContext GetAccessContext ()
+    {
+      return new ElementAccessContext(_literalExpression);
+    }
+
+    public ISymbolTable GetCompletionSymbolTable ()
+    {
+      return GetReferenceSymbolTable(false);
+    }
+  }
+
+  public static class SymbolTableExtensions
+  {
+    public static ISymbolTable Filter (this ISymbolTable symbolTable, Func<ISymbolInfo, bool> predicate)
+    {
+      return symbolTable.Filter(new PredicateFilter(predicate));
+    }
+  }
+}
---
+++ src/TestFx.ReSharper/References/MemberReferenceFactory.cs
@@ -0,0 +1,100 @@
+﻿// Copyright 2015, 2014 Matthias Koch
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq;
+using JetBrains.Annotations;
+using JetBrains.Metadata.Reader.Impl;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Resolve;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.Util;
+using TestFx.ReSharper.Utilities.Psi;
+using TestFx.Utilities;
+
+namespace TestFx.ReSharper.References
+{
+  public class MemberReferenceFactory : IReferenceFactory
+  {
+    private static readonly ClrTypeName s_memberReferenceAttribute = new ClrTypeName("JetBrains.Annotations.MemberReferenceAttribute");
+
+    public IReference[] GetReferences (ITreeNode element, IReference[] oldReferences)
+    {
+      var literalExpression = element as ICSharpLiteralExpression;
+      if (literalExpression == null || !(literalExpression.ConstantValue.Value is string) || literalExpression.ConstantValue.IsBadValue())
+        return EmptyArray<IReference>.Instance;
+
+      if (IsValid(literalExpression, oldReferences))
+        return oldReferences;
+
+      var type = GetRelatedTypeElementFromAttributeUsage(literalExpression);
+      if (type == null)
+        return EmptyArray<IReference>.Instance;
+
+      return new IReference[] { new MemberReference(literalExpression, type) };
+    }
+
+    public bool HasReference (ITreeNode element, IReferenceNameContainer names)
+    {
+      var literalExpression = element as ILiteralExpression;
+      if (literalExpression == null)
+        return false;
+
+      var referenceName = literalExpression.ConstantValue.Value as string;
+      return referenceName != null && names.Contains(referenceName);
+    }
+
+    [ContractAnnotation ("oldReferences: null => false")]
+    private bool IsValid (ICSharpLiteralExpression literalExpression, IReference[] oldReferences)
+    {
+      if (oldReferences == null)
+        return false;
+
+      var oldReference = oldReferences.Cast<MemberReference>().SingleOrDefault();
+      if (oldReference == null)
+        return false;
+
+      var referenceName = literalExpression.ConstantValue.Value as string;
+      return oldReference.IsValid() && oldReference.GetName() == referenceName;
+    }
+
+    [CanBeNull]
+    private ITypeElement GetRelatedTypeElementFromAttributeUsage (ICSharpLiteralExpression literalExpression)
+    {
+      var attribute = AttributeNavigator.GetByConstructorArgumentExpression(literalExpression);
+      if (attribute == null)
+        return null;
+
+      var argument = (ICSharpArgument) literalExpression.Parent.AssertNotNull();
+      if (argument.MatchingParameter == null)
+        return null;
+
+      var parameter = argument.MatchingParameter.Element;
+      if (parameter.HasAttributeInstance(s_memberReferenceAttribute, false))
+        return null;
+
+      var constructor = attribute.ConstructorReference.GetResolved<IConstructor>().AssertNotNull();
+      var literalArgumentIndex = constructor.Parameters.IndexOf(parameter);
+      if (literalArgumentIndex == 0)
+        return null;
+
+      var typeofExpression = attribute.Arguments[literalArgumentIndex - 1].Expression as ITypeofExpression;
+      if (typeofExpression == null)
+        return null;
+
+      return ((IDeclaredType) typeofExpression.ArgumentType).GetTypeElement();
+    }
+  }
+}
