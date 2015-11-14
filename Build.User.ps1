3c740bd2aa487793f498baf988437d16677f059b
Matthias Koch <ithrowexceptions@gmail.com>
2015-11-14 22:59:28 +0100

    Upgraded build script to msbuild 12.0 and dotcover wave04.

--- Build.User.ps1
+++ Build.User.ps1
@@ -1,3 +1,3 @@
-$LocalMsBuildDir  = "C:\Windows\Microsoft.NET\Framework\v4.0.30319"
+$LocalMsBuildDir  = "C:\Program Files (x86)\MSBuild\12.0\Bin"
 $LocalNuGetDir    = "$PSScriptRoot\.."
-$LocalDotCoverDir = "C:\Users\$env:USERNAME\AppData\Local\JetBrains\Installations\dotCover02"
+$LocalDotCoverDir = "C:\Users\$env:USERNAME\AppData\Local\JetBrains\Installations\dotCover04"
