bed1dc0883b39f5bf04564712e4e556825ffb5a8
Matthias Koch <ithrowexceptions@gmail.com>
2016-02-16 19:47:58 +0100

    asd

--- Build.User.ps1
+++ Build.User.ps1
@@ -1,3 +1,4 @@
 $LocalMsBuildDir  = "C:\Program Files (x86)\MSBuild\12.0\Bin"
 $LocalNuGetDir    = "$PSScriptRoot\.."
 $LocalDotCoverDir = "C:\Users\$env:USERNAME\AppData\Local\JetBrains\Installations\dotCover04"
+
