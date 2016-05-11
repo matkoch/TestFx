eea72a5a498af75524ed52fd30414ab5cb7bfd85
Matthias Koch <ithrowexceptions@gmail.com>
2016-05-11 11:31:17 +0200

    Changed to dotCover05.

--- Build.User.ps1
+++ Build.User.ps1
@@ -1,4 +1,4 @@
 $LocalMsBuildDir  = "C:\Program Files (x86)\MSBuild\12.0\Bin"
 $LocalNuGetDir    = "$PSScriptRoot\.."
-$LocalDotCoverDir = "C:\Users\$env:USERNAME\AppData\Local\JetBrains\Installations\dotCover04"
+$LocalDotCoverDir = "C:\Users\$env:USERNAME\AppData\Local\JetBrains\Installations\dotCover05"
 
