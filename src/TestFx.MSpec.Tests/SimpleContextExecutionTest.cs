7f8994563fc69063edf6cd9eb47992d779a17a3f
Matthias Koch <ithrowexceptions@gmail.com>
2016-06-14 18:38:13 +0200

    Removed ContextTestBase since it is not executable when debugging with ReSharper.

--- src/TestFx.MSpec.Tests/SimpleContextExecutionTest.cs
+++ src/TestFx.MSpec.Tests/SimpleContextExecutionTest.cs
@@ -13,32 +13,89 @@
 // limitations under the License.
 
 using System;
+using System.Linq;
 using FakeItEasy;
 using FakeItEasy.Core;
+using Machine.Specifications;
 using TestFx.Evaluation.Results;
+using TestFx.TestInfrastructure;
 
 namespace TestFx.MSpec.Tests
 {
-  internal class SimpleContextExecutionTest : ContextTestBase
+  internal class SimpleContext
+  {
+    public static Action OuterContextBaseEstablish = A.Fake<Action>();
+    public static Action OuterContextEstablish = A.Fake<Action>();
+    public static Action BaseContextBaseEstablish = A.Fake<Action>();
+    public static Action BaseContextEstablish = A.Fake<Action>();
+    public static Action Establish = A.Fake<Action>();
+    public static Action Action = A.Fake<Action>();
+    public static Action Assertion = A.Fake<Action>();
+    public static Action Cleanup = A.Fake<Action>();
+    public static Action BaseContextCleanup = A.Fake<Action>();
+    public static Action BaseContextBaseCleanup = A.Fake<Action>();
+    public static Action OuterContextCleanup = A.Fake<Action>();
+    public static Action OuterContextBaseCleanup = A.Fake<Action>();
+
+    [Subject (typeof(int))]
+    internal class outer_context : outer_context_base
+    {
+      internal class when_actioning : base_context
+      {
+        Establish ctx = () => Establish();
+
+        Because of = () => Action();
+
+        It asserts = () => Assertion();
+
+        Cleanup stuff = () => Cleanup();
+      }
+
+      Establish ctx = () => OuterContextEstablish();
+
+      Cleanup stuff = () => OuterContextCleanup();
+    }
+
+    internal class outer_context_base
+    {
+      Establish ctx = () => OuterContextBaseEstablish();
+
+      Cleanup stuff = () => OuterContextBaseCleanup();
+    }
+
+    internal class base_context : base_context_base
+    {
+      Establish ctx = () => BaseContextEstablish();
+
+      Cleanup stuff = () => BaseContextCleanup();
+    }
+
+    internal class base_context_base
+    {
+      Establish ctx = () => BaseContextBaseEstablish();
+
+      Cleanup stuff = () => BaseContextBaseCleanup();
+    }
+  }
+
+  internal class SimpleContextExecutionTest : TestBase<SimpleContext.outer_context.when_actioning>
   {
     protected override void AssertResults (IRunResult runResult, IFakeScope scope)
     {
       using (scope.OrderedAssertions())
       {
-        A.CallTo(() => OuterContextBaseEstablish()).MustHaveHappened();
-        A.CallTo(() => OuterContextEstablish()).MustHaveHappened();
-        A.CallTo(() => BaseContextBaseEstablish()).MustHaveHappened();
-        A.CallTo(() => BaseContextEstablish()).MustHaveHappened();
-        A.CallTo(() => Establish()).MustHaveHappened();
-
-        A.CallTo(() => Action()).MustHaveHappened();
-        A.CallTo(() => Assertion()).MustHaveHappened();
-
-        A.CallTo(() => Cleanup()).MustHaveHappened();
-        A.CallTo(() => BaseContextCleanup()).MustHaveHappened();
-        A.CallTo(() => BaseContextBaseCleanup()).MustHaveHappened();
-        A.CallTo(() => OuterContextCleanup()).MustHaveHappened();
-        A.CallTo(() => OuterContextBaseCleanup()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.OuterContextBaseEstablish()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.OuterContextEstablish()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.BaseContextBaseEstablish()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.BaseContextEstablish()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.Establish()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.Action()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.Assertion()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.Cleanup()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.BaseContextCleanup()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.BaseContextBaseCleanup()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.OuterContextCleanup()).MustHaveHappened();
+        A.CallTo(() => SimpleContext.OuterContextBaseCleanup()).MustHaveHappened();
       }
     }
   }
--- src/TestFx.MSpec.Tests/ThrowingContextExecutionTest.cs
+++ src/TestFx.MSpec.Tests/ThrowingContextExecutionTest.cs
@@ -13,38 +13,88 @@
 // limitations under the License.
 
 using System;
+using System.Linq;
 using FakeItEasy;
 using FakeItEasy.Core;
+using Machine.Specifications;
 using TestFx.Evaluation.Results;
+using TestFx.TestInfrastructure;
 
 namespace TestFx.MSpec.Tests
 {
-  internal class ThrowingContextExecutionTest : ContextTestBase
+  internal class ThrowingContext
   {
-    public override void SetUp ()
-    {
-      base.SetUp();
+    public static Action OuterContextBaseEstablish = A.Fake<Action>();
+    public static Action OuterContextEstablish = A.Fake<Action>();
+    public static Action BaseContextBaseEstablish = () => { throw new Exception(); };
+    public static Action BaseContextEstablish = A.Fake<Action>();
+    public static Action Establish = A.Fake<Action>();
+    public static Action Action = A.Fake<Action>();
+    public static Action Assertion = A.Fake<Action>();
+    public static Action Cleanup = A.Fake<Action>();
+    public static Action BaseContextCleanup = A.Fake<Action>();
+    public static Action BaseContextBaseCleanup = A.Fake<Action>();
+    public static Action OuterContextCleanup = A.Fake<Action>();
+    public static Action OuterContextBaseCleanup = A.Fake<Action>();
 
-      BaseContextBaseEstablish = ThrowingAction;
+    [Subject (typeof(int))]
+    internal class outer_context : outer_context_base
+    {
+      internal class when_actioning : base_context
+      {
+        Establish ctx = () => Establish();
+
+        Because of = () => Action();
+
+        It asserts = () => Assertion();
+
+        Cleanup stuff = () => Cleanup();
+      }
+
+      Establish ctx = () => OuterContextEstablish();
+
+      Cleanup stuff = () => OuterContextCleanup();
     }
 
+    internal class outer_context_base
+    {
+      Establish ctx = () => OuterContextBaseEstablish();
+
+      Cleanup stuff = () => OuterContextBaseCleanup();
+    }
+
+    internal class base_context : base_context_base
+    {
+      Establish ctx = () => BaseContextEstablish();
+
+      Cleanup stuff = () => BaseContextCleanup();
+    }
+
+    internal class base_context_base
+    {
+      Establish ctx = () => BaseContextBaseEstablish();
+
+      Cleanup stuff = () => BaseContextBaseCleanup();
+    }
+  }
+
+  internal class ThrowingContextExecutionTest : TestBase<ThrowingContext.outer_context.when_actioning>
+  {
     protected override void AssertResults (IRunResult runResult, IFakeScope scope)
     {
       using (scope.OrderedAssertions())
       {
-        A.CallTo(() => OuterContextBaseEstablish()).MustHaveHappened();
-        A.CallTo(() => OuterContextEstablish()).MustHaveHappened();
-
-        A.CallTo(() => BaseContextEstablish()).MustNotHaveHappened();
-        A.CallTo(() => Establish()).MustNotHaveHappened();
-        A.CallTo(() => Action()).MustNotHaveHappened();
-        A.CallTo(() => Assertion()).MustNotHaveHappened();
-        A.CallTo(() => Cleanup()).MustNotHaveHappened();
-        A.CallTo(() => BaseContextCleanup()).MustNotHaveHappened();
-        A.CallTo(() => BaseContextBaseCleanup()).MustNotHaveHappened();
-
-        A.CallTo(() => OuterContextCleanup()).MustHaveHappened();
-        A.CallTo(() => OuterContextBaseCleanup()).MustHaveHappened();
+        A.CallTo(() => ThrowingContext.OuterContextBaseEstablish()).MustHaveHappened();
+        A.CallTo(() => ThrowingContext.OuterContextEstablish()).MustHaveHappened();
+        A.CallTo(() => ThrowingContext.BaseContextEstablish()).MustNotHaveHappened();
+        A.CallTo(() => ThrowingContext.Establish()).MustNotHaveHappened();
+        A.CallTo(() => ThrowingContext.Action()).MustNotHaveHappened();
+        A.CallTo(() => ThrowingContext.Assertion()).MustNotHaveHappened();
+        A.CallTo(() => ThrowingContext.Cleanup()).MustNotHaveHappened();
+        A.CallTo(() => ThrowingContext.BaseContextCleanup()).MustNotHaveHappened();
+        A.CallTo(() => ThrowingContext.BaseContextBaseCleanup()).MustNotHaveHappened();
+        A.CallTo(() => ThrowingContext.OuterContextCleanup()).MustHaveHappened();
+        A.CallTo(() => ThrowingContext.OuterContextBaseCleanup()).MustHaveHappened();
       }
     }
   }
