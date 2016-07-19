38b25fc4b0075fc7cc092a97fcbbf8a8ae2cf1ff
Matthias Koch <ithrowexceptions@gmail.com>
2016-07-19 06:29:50 +0200

    Fixed NugetDir.

--- Build.User.ps1
+++ Build.User.ps1
@@ -1,4 +1,4 @@
 $LocalMsBuildDir  = "C:\Program Files (x86)\MSBuild\12.0\Bin"
-$LocalNuGetDir    = "$PSScriptRoot\.."
+$LocalNuGetDir    = "$PSScriptRoot\shared\build\tools"
 $LocalDotCoverDir = "C:\Users\$env:USERNAME\AppData\Local\JetBrains\Installations\dotCover05"
 
